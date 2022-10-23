9ef3f357602d637c059e3fafef887a52bc634416
ryudongjae <rdj10149@gmail.com>
2022-10-23 18:03:14 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Test from "./Test";
 function App() {
-  njonionioknkoinok
+    return (
+     <div>
+        {/* Test라는 컴포넌트를 내보낸다. */}
+        <Test/>
+     </div>
+    );
 }
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+//리엑트 컴포넌트 만들때 
+import React from "react";
+
+function Test(){
+    return <div> 테스트 </div>
+}
+
+//Test 컴포넌터를 내보낸다. 다른 컴포넌트에서 불러와서 사용이 가능하다.
+export default Test;
