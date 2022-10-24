0cfbb2ffbc812e76a08cbd6de1f8737f69c2bf6d
ryudongjae <rdj10149@gmail.com>
2022-10-24 22:49:29 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Counter from "./Counter";
 import Test from "./Test";
 import Wrapper from "./Wrapper";
 
 
 function App() {
     return (
-      <Wrapper>
-        {/* Test라는 컴포넌트를 내보낸다. */}
-        <Test name="react1" color="blue" isSpecial={true}/>
-        <Test color="red" cisSpecial={false}/>
-        <Test name="react2" isSpecial={false}/>
-      </Wrapper>
+      // <Wrapper>
+      //   {/* Test라는 컴포넌트를 내보낸다. */}
+      //   <Test name="react1" color="blue" isSpecial={true}/>
+      //   <Test color="red" cisSpecial={false}/>
+      //   <Test name="react2" isSpecial={false}/>
+      // </Wrapper>
+
+      <Counter/>
     );
 }
 
